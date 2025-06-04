895f86fb4a52884d266f332ba572b1a0d43e81a3
chubbyhippo <mkmsacc@outlook.com>
2025-06-04 09:37:40 +0700

    Add `TimeChecker` component to determine if it's after 12 PM

---
+++ src/main/java/io/github/chubbyhippo/demo/TimeChecker.java
@@ -0,0 +1,11 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeChecker {
+
+    public boolean isAfter12Pm() {
+        return true;
+    }
+}
