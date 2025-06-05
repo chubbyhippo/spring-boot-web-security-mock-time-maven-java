875f4d7107bc0288440f0eb5049f7d088db849b3
chubbyhippo <mkmsacc@outlook.com>
2025-06-05 13:53:24 +0700

    Update `DemoApplicationTests` to include time-based access control test logic

--- src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
@@ -5,26 +5,38 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
+import java.time.LocalTime;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Import(SecurityConfig.class)
 class DemoApplicationTests {
 
-	@Autowired
-	private MockMvcTester mockMvcTester;
+    @Autowired
+    private MockMvcTester mockMvcTester;
 
-	@Test
-	@DisplayName("should return hello")
-	@WithMockUser
-	void shouldReturnHello() {
-		mockMvcTester.get()
-				.uri("/hello")
-				.assertThat()
-				.hasStatusOk()
-				.bodyText()
-				.isEqualTo("Hello!");
-	}
+    @Test
+    @DisplayName("should return hello")
+    @WithMockUser
+    void shouldReturnHelloOr403() {
+        if (LocalTime.now().isAfter(LocalTime.of(12, 0))){
+            mockMvcTester.get()
+                    .uri("/hello")
+                    .assertThat()
+                    .hasStatusOk()
+                    .bodyText()
+                    .isEqualTo("Hello!");
+        } else {
+            mockMvcTester.get()
+                    .uri("/hello")
+                    .assertThat()
+                    .hasStatus4xxClientError();
+        }
+
+    }
 
 }
