80e70538f813cd57decbe0ee661e8a33e031ea25
chubbyhippo <mkmsacc@outlook.com>
2025-06-05 10:57:02 +0700

    Refactor `SecurityConfig` to inject `TimeChecker` via constructor

--- src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
@@ -6,12 +6,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class SecurityConfig {
+    private final TimeChecker timeChecker;
+
+    public SecurityConfig(TimeChecker timeChecker) {
+        this.timeChecker = timeChecker;
+    }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http, TimeChecker timeChecker) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(registry ->
                 registry.anyRequest()
