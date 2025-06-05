dc0d7c544e309229b2d1934ea2d47b25dbc62859
chubbyhippo <mkmsacc@outlook.com>
2025-06-05 13:52:56 +0700

    Fix `TimeChecker.isAfter12Pm` to return actual time-based result

--- src/main/java/io/github/chubbyhippo/demo/TimeChecker.java
+++ src/main/java/io/github/chubbyhippo/demo/TimeChecker.java
@@ -2,10 +2,12 @@ package io.github.chubbyhippo.demo;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Component
 public class TimeChecker {
 
-    public boolean isAfter12Pm() {
-        return true;
+    public Boolean isAfter12Pm() {
+        return LocalTime.now().isAfter(LocalTime.of(12, 0));
     }
 }
