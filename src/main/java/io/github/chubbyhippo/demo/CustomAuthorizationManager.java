5c324d27790c5ff6cdefac78a4f8fe0404daebe2
chubbyhippo <mkmsacc@outlook.com>
2025-07-23 14:36:31 +0700

    Add `CustomAuthorizationManager` for time-based access control logic

---
+++ src/main/java/io/github/chubbyhippo/demo/CustomAuthorizationManager.java
@@ -0,0 +1,18 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.time.LocalTime;
+import java.util.function.Supplier;
+
+public class CustomAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+
+    @Override
+    public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        boolean isAfterMidday = LocalTime.now().isAfter(LocalTime.of(12, 0));
+        return new AuthorizationDecision(isAfterMidday);
+    }
+}
