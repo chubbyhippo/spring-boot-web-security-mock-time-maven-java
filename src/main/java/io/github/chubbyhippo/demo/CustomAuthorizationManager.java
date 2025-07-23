ee6e7ffaa40b23c2463586f4f73030ecadf6ed42
chubbyhippo <mkmsacc@outlook.com>
2025-07-23 14:48:07 +0700

    Add `Clock` dependency to `CustomAuthorizationManager` for improved testability

--- src/main/java/io/github/chubbyhippo/demo/CustomAuthorizationManager.java
+++ src/main/java/io/github/chubbyhippo/demo/CustomAuthorizationManager.java
@@ -5,10 +5,17 @@ import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import java.time.Clock;
 import java.time.LocalTime;
 import java.util.function.Supplier;
 
 public class CustomAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+
+    private final Clock clock;
+
+    public CustomAuthorizationManager(Clock clock) {
+        this.clock = clock;
+    }
 
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
