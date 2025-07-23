699ccc9bc9b5b2ae99aafcf0b2cf4147e6e48b66
chubbyhippo <mkmsacc@outlook.com>
2025-07-23 15:05:16 +0700

    Update `DemoApplicationTests` to use `Clock` for time-dependent assertions and remove redundant test class

--- src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
@@ -2,50 +2,46 @@ package io.github.chubbyhippo.demo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestPropertySource(properties = "expression='true'")
 class WhenExpressionIsTrueTest {
 
     @Autowired
     private MockMvcTester mockMvcTester;
+    @MockitoBean
+    private Clock clock;
+
+    private void setClock(String time) {
+        var instant = Instant.parse("2025-07-23T" + time + "Z");
+        var zone = ZoneId.of("UTC");
+        var fixedClock = Clock.fixed(instant, zone);
+        Mockito.when(clock.instant()).thenReturn(fixedClock.instant());
+        Mockito.when(clock.getZone()).thenReturn(fixedClock.getZone());
+    }
 
     @Test
     @DisplayName("should return hello")
     @WithMockUser
     void shouldReturnHello() {
+
+        setClock("14:00:00.00");
         mockMvcTester.get()
                 .uri("/hello")
                 .assertThat()
                 .hasStatusOk()
                 .bodyText()
                 .isEqualTo("Hello!");
-    }
-
-}
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@TestPropertySource(properties = "expression='false'")
-class WhenExpressionIsFalseTest {
-
-    @Autowired
-    private MockMvcTester mockMvcTester;
-
-    @Test
-    @DisplayName("should return 403")
-    @WithMockUser
-    void shouldReturn403() {
-        mockMvcTester.get()
-                .uri("/hello")
-                .assertThat()
-                .hasStatus4xxClientError();
     }
 }
