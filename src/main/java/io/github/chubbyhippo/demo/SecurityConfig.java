243b03c5ba72659f4261ac5d0d4fcc7928a2bedc
chubbyhippo <mkmsacc@outlook.com>
2025-07-23 14:36:19 +0700

    Refactor `SecurityConfig` to replace `WebExpressionAuthorizationManager` with `CustomAuthorizationManager`

--- src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
@@ -1,25 +1,20 @@
 package io.github.chubbyhippo.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 public class SecurityConfig {
-
-    @Value("${expression}")
-    private String expression;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(registry ->
                 registry.anyRequest()
-                        .access(new WebExpressionAuthorizationManager(expression))
+                        .access(new CustomAuthorizationManager())
         );
         return http.build();
     }
