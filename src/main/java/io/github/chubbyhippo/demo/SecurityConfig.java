48adeaea42d9d3fe8c6b2a05d8dbb6dd85a7cb3d
chubbyhippo <mkmsacc@outlook.com>
2025-07-23 14:48:23 +0700

    Add `Clock` bean to `SecurityConfig` and inject into `CustomAuthorizationManager`

--- src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/SecurityConfig.java
@@ -6,15 +6,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.time.Clock;
+
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    Clock clock() {
+        return Clock.systemDefaultZone();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(registry ->
                 registry.anyRequest()
-                        .access(new CustomAuthorizationManager())
+                        .access(new CustomAuthorizationManager(clock()))
         );
         return http.build();
     }
